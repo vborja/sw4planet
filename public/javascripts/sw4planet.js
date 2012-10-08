d511b2a18072267d05757d51fdd55f805c0b96af
Victor Borja <vborja@apache.org>
2012-10-08 16:00:55 -0500

    Add layout for program

--- public/javascripts/sw4planet.js
+++ public/javascripts/sw4planet.js
@@ -8,13 +8,17 @@
                                  , { immediateRender:true,
                                      css:{ left: '-120%' }}
                                  , { css:{ left: '0px' }}),
-                100)
+                10)
 
     fx.pin( $('.are-you'), 1000, {
       offset: -150,
       onPin: function(){
+        $('#come .pin-spacer').height('1500px')
+        $('#come .pin-stop').css('top', '0px')
       },
       onUnpin: function(){
+        $('#come .pin-spacer').height('600px')
+        $('#come .pin-stop').css('top', '-700px')
       }
     })
 
@@ -47,6 +51,62 @@
                                       css:{ left: '120%' }}
                                   , { css:{ left: '0px'}})
                 ,300)
+
+
+    fx.pin('.friday', 1000, {
+      offset: -50,
+      anim: (new TimelineLite)
+       .append([
+         TweenMax.from( $('.friday .meet'), 1,
+                        { immediateRender: true, css: { left: '200%' } } )
+       ])
+       .append([
+         TweenMax.from( $('.friday .share'), 1,
+                        { immediateRender: true,css: { left: '-200%' } } )
+       ])
+       .append([
+         TweenMax.from( $('.friday .team'), 1,
+                        { immediateRender: true,css: { left: '200%' } } )
+       ])
+       .append([
+         TweenMax.from( $('.friday .work'), 1,
+                        { immediateRender: true,css: { left: '-200%' } } )
+       ])
+    })
+
+    fx.pin('.saturday', 1000, {
+      offset: -50,
+      anim: (new TimelineLite)
+       .append([
+         TweenMax.from( $('.saturday .validate'), 1,
+                        { immediateRender: true,css: { left: '-200%' } } )
+       ])
+       .append([
+         TweenMax.from( $('.saturday .build'), 1,
+                        { immediateRender: true,css: { left: '200%' } } )
+       ])
+       .append([
+         TweenMax.from( $('.saturday .mentor'), 1,
+                        { immediateRender: true,css: { left: '-200%' } } )
+       ])
+    })
+
+    fx.pin('.sunday', 1000, {
+      offset: -50,
+      anim: (new TimelineLite)
+       .append([
+         TweenMax.from( $('.sunday .finish'), 1,
+                        { immediateRender: true,css: { left: '-200%' } } )
+       ])
+       .append([
+         TweenMax.from( $('.sunday .pitch'), 1,
+                        { immediateRender: true,css: { left: '-200%' } } )
+       ])
+       .append([
+         TweenMax.from( $('.sunday .win'), 1,
+                        { immediateRender: true,css: { left: '200%' } } )
+       ])
+    })
 
 
   }
